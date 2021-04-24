e47ae58638b7f3f898be89d01f7d744f9c5c37d4
MunawarAliAraiz <81907855+MunawarAliAraiz@users.noreply.github.com>
2021-04-24 14:58:54 +0500

    Make Classes

---
+++ src/stringmanipulation/Task1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringmanipulation;
+
+/**
+ *
+ * @author Munawar Ali
+ */
+public class Task1 {
+    
+    //public int wordCounter(String s)
+    {
+       
+    }
+    
+}
